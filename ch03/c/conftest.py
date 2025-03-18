7582a7731d77067b292525a4f846011d0030cc3b
Larry Jones <mrwizard82d1@gmail.com>
2025-03-18 17:45:46 -0500

    Use multiple fixtures per test or fixture

---
+++ ch03/c/conftest.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
+
+import cards
+
+
+@pytest.fixture(scope='session')
+def some_cards():
+    """List of different Card objects."""
+    return [
+        cards.Card('write book', 'Brian', 'done'),
+        cards.Card('edit book', 'Katie', 'done'),
+        cards.Card('write 2nd edition', 'Brian', 'todo'),
+        cards.Card('edit 2nd edition', 'Katie', 'todo'),
+    ]
+
+
+@pytest.fixture(scope='function')
+def cards_db():
+    """CardsDB object conneected to a temporary database."""
+    with TemporaryDirectory() as db_dir:
+        db_path = Path(db_dir)
+        db = cards.CardsDB(db_path)
+
+        yield db
+
+        db.close()
+
+
+@pytest.fixture(scope='function')
+def non_empty_db(cards_db, some_cards):
+    """CardsDB object populated with `some_cards`."""
+    for c in some_cards:
+        cards_db.add_card(c)
+
+    return cards_db
