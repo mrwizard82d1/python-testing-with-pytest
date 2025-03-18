588e891ad54feb33b818efc23816f34868b9dff8
Larry Jones <mrwizard82d1@gmail.com>
2025-03-18 16:07:36 -0500

    Remove commented out code (sigh...)

--- ch03/test_count.py
+++ ch03/test_count.py
@@ -14,18 +14,12 @@ def cards_db():
         db = cards.CardsDB(db_path)
 
         # When the `yield` is executed, the test runs with `db` as its fixture
-        # print('Yielding to a test')
         yield db
 
         # The code **after** the `yield` statement is our test teardown. It is
         # guaranteed to run even if an Exception is raised by our test code.
-        # print('Closing the database')
         db.close()
 
 
 def test_empty(cards_db):
     assert cards_db.count() == 0
-
-
-# def test_exception(cards_db):
-#     raise Exception('Demonstrates clean up after test')
