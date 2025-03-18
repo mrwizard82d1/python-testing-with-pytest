38c3bee1b2bac2f4accac2d6926dd1706929042d
Larry Jones <mrwizard82d1@gmail.com>
2025-03-18 16:06:47 -0500

    Revert "Demonstrate fixture clean up after exception"
    This reverts commit b7e378a9be0c772adbb55e1e60ed6c27291da68f.

--- ch03/test_count.py
+++ ch03/test_count.py
@@ -14,12 +14,12 @@ def cards_db():
         db = cards.CardsDB(db_path)
 
         # When the `yield` is executed, the test runs with `db` as its fixture
-        print('Yielding to a test')
+        # print('Yielding to a test')
         yield db
 
         # The code **after** the `yield` statement is our test teardown. It is
         # guaranteed to run even if an Exception is raised by our test code.
-        print('Closing the database')
+        # print('Closing the database')
         db.close()
 
 
@@ -27,5 +27,5 @@ def test_empty(cards_db):
     assert cards_db.count() == 0
 
 
-def test_exception(cards_db):
-    raise Exception('Demonstrates clean up after test')
+# def test_exception(cards_db):
+#     raise Exception('Demonstrates clean up after test')
